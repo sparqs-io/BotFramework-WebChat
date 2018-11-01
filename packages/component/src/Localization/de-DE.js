99c578b5fc6bc98271caac59942a097011f18273
Leon Jäkel <info@leon-jaekel.com>
2018-11-01 21:18:10 +0100

    Changes the localization props for german language
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   packages/component/src/Localization/de-DE.js

--- packages/component/src/Localization/de-DE.js
+++ packages/component/src/Localization/de-DE.js
@@ -1,3 +1,28 @@
+function xMinutesAgo(date) {
+  const now = Date.now();
+  const deltaInMs = now - new Date(date).getTime();
+  const deltaInMinutes = Math.floor(deltaInMs / 60000);
+  const deltaInHours = Math.floor(deltaInMs / 3600000);
+
+  if (deltaInMinutes < 1) {
+    return 'Jetzt gerade';
+  } else if (deltaInMinutes === 1) {
+    return 'Vor einer Minute';
+  } else if (deltaInHours < 1) {
+    return `Vor ${ deltaInMinutes } Minuten`;
+  } else if (deltaInHours === 1) {
+    return `Vor einer Stunde`;
+  } else if (deltaInHours < 5) {
+    return `Vor ${ deltaInHours } Stunden`;
+  } else if (deltaInHours <= 24) {
+    return `Heute`;
+  } else if (deltaInHours <= 48) {
+    return `Gestern`;
+  } else {
+    return new Intl.DateTimeFormat('de-DE').format(date);
+  }
+}
+
 export default {
   'Chat': 'Chat',
   'Listening…': 'Hören…',
@@ -5,12 +30,12 @@ export default {
   'Send failed, {retry}': 'konnte nicht senden, {retry}',
   'Send': 'Senden',
   'Sending': 'sendet',
-  // 'Speak': '',
-  // 'Starting…': '',
+  'Speak': 'Sprechen',
+  'Starting…': 'startet...',
   'Tax': 'MwSt',
   'Total': 'Gesamtbetrag',
   'Type your message': 'Verfasse eine Nachricht',
-  // 'Upload file': '',
-  'VAT': 'VAT'
-  // 'X minutes ago':
+  'Upload file': 'Datei hochladen',
+  'VAT': 'VAT',
+  'X minutes ago': xMinutesAgo
 }
